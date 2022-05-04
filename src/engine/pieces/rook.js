380aeca0b13e8a7024c49c00c121677c5ca7bd14
Patricia Robles <patriciarobles9@gmail.com>
2022-05-04 15:32:43 +0100

    rook logic

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,21 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let array = []
+
+        for (let i = 0; i <= 7; i++) {
+            array.push(Square.at(location.row, i))
+        }
+        
+        for (let i = 0; i <= 7; i++) {
+            array.push(Square.at(i, location.col))
+        }
+        
+        let filteredArray = array.filter(square => square.row !== location.row || square.col !== location.col);
+        return filteredArray;
     }
+    
 }
+
+
