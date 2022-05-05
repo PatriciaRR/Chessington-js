bca7b307874aaaf1119d41a21daccfc50b0cc93d
Patricia Robles <patriciarobles9@gmail.com>
2022-05-05 14:21:31 +0100

    knight logic

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,6 +1,5 @@
 import Piece from './piece';
 import Square from '../square';
-
 export default class Knight extends Piece {
     constructor(player) {
         super(player);
@@ -22,7 +21,7 @@ export default class Knight extends Piece {
             array.push(Square.at(location.row -1, location.col +2 ))
             array.push(Square.at(location.row -2, location.col +1 ))
 
-
-        return array;
+            let filteredArray = array.filter(square => square.col >= 0 && square.row >= 0 && square.col <= 7 && square.row <= 7);
+            return filteredArray;
     }
 }
