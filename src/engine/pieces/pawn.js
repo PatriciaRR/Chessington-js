d5396cc7d986a42e7ebaf7541aac586473ce4220
Patricia Robles <patriciarobles9@gmail.com>
2022-05-05 14:17:56 +0100

    bishop blocking

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -22,22 +22,19 @@ export default class Pawn extends Piece {
         } else {
             let hasPieceInFront = board.getPiece(Square.at(location.row - 1, location.col));
             let hasPieceTwoInFront = board.getPiece(Square.at(location.row - 2, location.col));
-            if (location.row < 6 && !hasPieceInFront){
-                array.push(Square.at(location.row - 1, location.col)) 
-            } else if (location.row === 6 && !hasPieceInFront) {
-                array.push(Square.at(location.row - 1, location.col));
-                if (!hasPieceTwoInFront){
-                    array.push(Square.at(location.row - 2, location.col));
-                }
-            }
-        }
-        
-        return array
-    }
-}
+                if (!hasPieceInFront){
+                    array.push(Square.at(location.row - 1, location.col)); 
+                } else if (location.row === 6 && !hasPieceTwoInFront) {
+                        array.push(Square.at(location.row - 2, location.col));
+                    }
+            } return array
+        } 
+    }       
 
-// if (!hasPieceInFront){
-//     array.push(Square.at(location.row - 1, location.col)) 
-// } else if 
-//     if (location.row === 6 && !hasPieceTwoInFront){
-//         array.push(Square.at(location.row - 2, location.col));
+// if (location.row < 6 && !hasPieceInFront){
+            //     array.push(Square.at(location.row - 1, location.col)) 
+            // } else if (location.row === 6 && !hasPieceInFront) {
+            //     array.push(Square.at(location.row - 1, location.col));
+            //     if (!hasPieceTwoInFront){
+            //         array.push(Square.at(location.row - 2, location.col));
+            //     }
