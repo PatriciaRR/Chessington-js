d37b04a3607df7c589fb7c38c032bde84f89a9d2
Patricia Robles <patriciarobles9@gmail.com>
2022-05-05 11:57:47 +0100

    Knight logic

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,22 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let array = []
+
+            array.push(Square.at(location.row + 2, location.col -1 ))
+            array.push(Square.at(location.row + 1, location.col -2 ))
+
+            array.push(Square.at(location.row + 1, location.col +2 ))
+            array.push(Square.at(location.row + 2, location.col +1 ))
+
+            array.push(Square.at(location.row -1, location.col -2 ))
+            array.push(Square.at(location.row -2, location.col -1 ))
+
+            array.push(Square.at(location.row -1, location.col +2 ))
+            array.push(Square.at(location.row -2, location.col +1 ))
+
+
+        return array;
     }
 }
